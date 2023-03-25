59ce5ef02a93211a3c9db54de4c0e3136e9a0d64
Zahid Kizmaz <tech@zahid.rocks>
2023-03-25 22:31:52 +0100

    Add basic decimal parsing

---
+++ decimify/parser.py
@@ -0,0 +1,5 @@
+from decimal import Decimal
+
+
+def parse(str_number: str) -> Decimal:
+    return Decimal(str_number)
---
+++ tests/test_parser.py
@@ -0,0 +1,33 @@
+from decimal import Decimal
+
+import pytest
+
+from decimify import parser
+
+
+class TestParse:
+    @pytest.mark.parametrize(
+        ("num", "expected"),
+        [
+            ("-12", Decimal("-12")),
+            ("0", Decimal("0")),
+            ("12", Decimal("12")),
+            ("12123123213", Decimal("12123123213")),
+            ("-12123123213", Decimal("-12123123213")),
+        ],
+    )
+    def test_parse_int_string_should_be_ok(self, num: str, expected: Decimal):
+        assert parser.parse(num) == expected
+
+    @pytest.mark.parametrize(
+        ("num", "expected"),
+        [
+            ("-12.2", Decimal("-12.2")),
+            ("0.0", Decimal("0.0")),
+            ("12.2", Decimal("12.2")),
+            ("12123.123213", Decimal("12123.123213")),
+            ("-12123.123213", Decimal("-12123.123213")),
+        ],
+    )
+    def test_parse_float_string_should_be_ok(self, num: str, expected: Decimal):
+        assert parser.parse(num) == expected
