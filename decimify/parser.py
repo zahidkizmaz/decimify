dbaacc33562aec75f04e0ec2ceae0c8d093ba472
Zahid Kizmaz <tech@zahid.rocks>
2023-04-14 21:50:17 +0200

    add tests and cover some cases of parsing

--- decimify/parser.py
+++ decimify/parser.py
@@ -2,4 +2,32 @@ from decimal import Decimal
 
 
 def parse(str_number: str) -> Decimal:
+    seperators = {".", ","}
+    cleanup_characters = {" "}
+
+    if separator := _find_separator(str_number, seperators):
+        seperators.remove(separator)
+        cleanup_characters = cleanup_characters.union(seperators)
+        str_number = _cleanup(str_number, cleanup_characters)
+        str_number = str_number.replace(separator, ".")
+
     return Decimal(str_number)
+
+
+def _find_separator(str_number: str, seperators: set[str]) -> str:
+    found_separator = ""
+    found_separator_idx = -1
+    for separator in seperators:
+        separator_idx = str_number.rfind(separator)
+        if separator_idx > found_separator_idx:
+            found_separator_idx = separator_idx
+            found_separator = separator
+
+    return found_separator
+
+
+def _cleanup(str_number: str, clean_up_strings: set[str]) -> str:
+    for clean_up_string in clean_up_strings:
+        str_number = str_number.replace(clean_up_string, "")
+
+    return str_number
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -31,3 +31,32 @@ class TestParse:
     )
     def test_parse_float_string_should_be_ok(self, num: str, expected: Decimal):
         assert parser.parse(num) == expected
+
+    @pytest.mark.parametrize(
+        ("num", "expected"),
+        [
+            ("-12,2", Decimal("-12.2")),
+            ("0,0", Decimal("0.0")),
+            ("12,2", Decimal("12.2")),
+            ("12123,123213", Decimal("12123.123213")),
+            ("-12123,123213", Decimal("-12123.123213")),
+        ],
+    )
+    def test_parse_float_string_with_comma_separators(
+        self, num: str, expected: Decimal
+    ):
+        assert parser.parse(num) == expected
+
+    @pytest.mark.parametrize(
+        ("num", "expected"),
+        [
+            ("12,123.123213", Decimal("12123.123213")),
+            ("-12,123.123213", Decimal("-12123.123213")),
+            ("12.123,123213", Decimal("12123.123213")),
+            ("-12.123,123213", Decimal("-12123.123213")),
+        ],
+    )
+    def test_parse_float_string_with_dot_and_comma_separators(
+        self, num: str, expected: Decimal
+    ):
+        assert parser.parse(num) == expected
